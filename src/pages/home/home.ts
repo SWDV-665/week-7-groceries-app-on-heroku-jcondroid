a5357b02e73522aa55dabf5cbd3fbc944a940495
Justin Conway <jconway2@live.maryville.edu>
2020-11-22 12:13:11 -0700

    Pushing week 5 stuff. Initial test.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { ToastController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
 import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @Component({
   selector: 'page-home',
@@ -12,7 +13,7 @@ import { InputDialogServiceProvider } from '../../providers/input-dialog-service
 export class HomePage {
   title = "Grocery";
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public inputDialogService: InputDialogServiceProvider) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: GroceriesServiceProvider, public inputDialogService: InputDialogServiceProvider, public socialSharing: SocialSharing) {
 
   }
 
@@ -29,6 +30,23 @@ export class HomePage {
 
     toast.present();
     this.dataService.remove_item(index);
+  }
+
+  share_item(item, index) {
+    console.log("sharing item - ", index);
+    const toast = this.toastCtrl.create({
+      message: 'Sharing Item - ' + index + "...",
+      duration: 3000
+    });
+
+    toast.present();
+    let message = "Grocery Item: " + item.name + " - Quantity: " + item.quantity;
+    let subject = "Shared via my custom Groceries App";
+    this.socialSharing.share(message, subject).then(() => {
+      console.log("Shared successfully");
+    }).catch((error) => {
+      console.error("Error while sharing: ", error);
+    });
   }
 
   edit_item(item, index) {
